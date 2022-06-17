c2fc8b68bd0bcbf8c226c101a5e21241ac250f95
Frans <97081479+Fransssss@users.noreply.github.com>
2022-06-16 18:29:10 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# user input
+
+print("\n==User Input==\n")
+# user input
+name = str(input("Hey Champion! What is your name: "))
+age = int(input("Would you mind telling your age: "))
+color = str(input("What is your favorite color: "))
+tall = float(input("How tall are you going to be by the early of next year: "))
+career = str(input("What is your future dream career: "))
+
+# #output the all statement
+print("\nGreat! Your name is " + name)
+print("You are " + str(age) + " y/o")
+print("Your favorite color is " + color)
+print("You are going to be " + str(tall) + "ft tall by the early of the next year!")
+print("Your future career is going to be a " + career + "!")
